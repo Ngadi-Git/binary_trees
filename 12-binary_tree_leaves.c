7b3c8736e8b0d49a3ed1e1f50ced9989a2521f84
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-05 00:50:59 +0000

    All tasks

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -16,5 +16,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		leaves += binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
 	}
-	return (leaves);
+	return (leaves);
 }
